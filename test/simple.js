367a530e893c963be6603fd0a40c48a5d05e8ccc
Linus Unnebäck <linus@folkdatorn.se>
2014-09-17 18:47:47 +0200

    mocha tests

--- test/simple.js
+++ test/simple.js
@@ -1,41 +1,55 @@
-var autolinks = require('../');
 
 var assert = require('assert');
+var autolinks = require('../');
 
-test('no change',
-    {
-      html: 'no change',
-      markdown: 'no change'
-    }
-);
+var data = [{
+  input: 'no change',
+  html: 'no change',
+  markdown: 'no change'
+},{
+  input: 'a link to google: http://google.com here',
+  html: 'a link to google: <a href="http://google.com">http://google.com</a> here',
+  markdown: 'a link to google: [http://google.com](http://google.com) here'
+},{
+  input: 'a relative link to google: www.google.com here',
+  html: 'a relative link to google: <a href="http://www.google.com">www.google.com</a> here',
+  markdown: 'a relative link to google: [www.google.com](http://www.google.com) here'
+},{
+  input: 'a mailto link for dave@daveeddy.com here',
+  html: 'a mailto link for <a href="mailto:dave@daveeddy.com">dave@daveeddy.com</a> here',
+  markdown: 'a mailto link for [dave@daveeddy.com](mailto:dave@daveeddy.com) here'
+}];
 
-test('a link to google: http://google.com here',
-    {
-      html: 'a link to google: <a href="http://google.com">http://google.com</a> here',
-      markdown: 'a link to google: [http://google.com](http://google.com) here'
-    }
-);
+describe('autolinks', function () {
 
-test('a relative link to google: www.google.com here',
-    {
-      html: 'a relative link to google: <a href="http://www.google.com">www.google.com</a> here',
-      markdown: 'a relative link to google: [www.google.com](http://www.google.com) here'
-    }
-);
-
-test('a mailto link for dave@daveeddy.com here',
-    {
-      html: 'a mailto link for <a href="mailto:dave@daveeddy.com">dave@daveeddy.com</a> here',
-      markdown: 'a mailto link for [dave@daveeddy.com](mailto:dave@daveeddy.com) here'
-    }
-);
-
-function test(s, o) {
-  console.log('===> %s <===', s);
-  Object.keys(o).forEach(function(fmt) {
-    var a = autolinks(s, fmt);
-    console.log('== %s == conversion to %s', a, fmt);
-    assert.strictEqual(a, o[fmt]);
+  it('should throw on unknown format', function () {
+    assert.throws(function () {
+      autolinks('test string', 'blah');
+    });
   });
-  console.log('ok\n');
-}
+
+  it('should use default format', function () {
+    var output = autolinks('http://test.com/');
+    var expected = '<a href="http://test.com/">http://test.com/</a>';
+
+    assert.equal(output, expected);
+  });
+
+  var formats = ['html', 'markdown'];
+
+  formats.forEach(function (format) {
+    describe('#' + format, function () {
+
+      data.forEach(function (testCase) {
+        it('should parse ' + testCase.input, function () {
+
+          var output = autolinks(testCase.input, format);
+          assert.equal(output, testCase[format]);
+
+        });
+      });
+
+    });
+  });
+
+});
