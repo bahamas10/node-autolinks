17b94a7e48a2fafe5452bfb60f73dad61cc42edc
Linus Unnebäck <linus@folkdatorn.se>
2014-09-17 19:22:33 +0200

    allow custom format function

--- autolinks.js
+++ autolinks.js
@@ -11,29 +11,38 @@ var URIre = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|
 var incompleteURIre = /(^|[^\/])(www\.[^\s)>]+(\b|$))/gim;
 var mailre = /\w+@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
-var formats = ['markdown', 'html'];
+var formats = {
+  'html': function (title, url) {
+    return '<a href="' + url + '">' + title + '</a>';
+  },
+  'markdown': function (title, url) {
+    return '[' + title + '](' + url + ')';
+  }
+};
 
 module.exports = autolinks;
 
 function autolinks(s, format) {
+  var fn;
 
-  if (format === undefined) {
-    format = 'html';
+  switch (typeof format) {
+    case 'undefined':
+      fn = formats.html;
+      break;
+    case 'string':
+      fn = formats[format];
+      break;
+    case 'function':
+      fn = format;
+      break;
   }
 
-  switch (format) {
-    case 'markdown':
-      return s.replace(URIre, '[$&]($&)')
-              .replace(incompleteURIre, '$1[$2](http://$2)')
-              .replace(mailre, '[$&](mailto:$&)');
-      break;
-    case 'html':
-      return s.replace(URIre, '<a href="$&">$&</a>')
-              .replace(incompleteURIre, '$1<a href="http://$2">$2</a>')
-              .replace(mailre, '<a href="mailto:$&">$&</a>');
-      break;
-    default:
-      throw new Error('Unkown format: ' + format);
+  if (fn === undefined) {
+    throw new Error('Unkown format: ' + format);
   }
+
+  return s.replace(URIre, function ($0) { return fn($0, $0); })
+          .replace(incompleteURIre, function ($0, $1, $2) { return $1 + fn($2, 'http://' + $2); })
+          .replace(mailre, function ($0) { return fn($0, 'mailto:' + $0); });
 
 }
