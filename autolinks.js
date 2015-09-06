97d3ef30140d2b6dcdf2ccc0b48d2de6f6d5174c
Dionysis Zindros <dionyziz@gmail.com>
2015-09-06 04:13:58 +0300

    Add support for custom HTML attributes

--- autolinks.js
+++ autolinks.js
@@ -10,13 +10,22 @@
  *  - Linus Unnebäck <linus@folkdatorn.se>
  */
 
+var escape = require('escape-html');
+
 var URIre = /\b(?:https?|ftp):\/\/[a-z\u00c0-\u01bf0-9-+&@#\/%?=~_|!:,.;]*[a-z\u00c0-\u01bf0-9-+&@#\/%=~_|]/gim;
 var incompleteURIre = /(^|[^\/])(www\.[^\s)>]+(\b|$))/gim;
 var mailre = /\w+@[a-z\u00c0-\u01bf_]+?(?:\.[a-z\u00c0-\u01bf]{2,6})+/gim;
 
 var formats = {
-  'html': function (title, url) {
-    return '<a href="' + url + '">' + title + '</a>';
+  'html': function (title, url, opts) {
+    var attrs = [];
+    opts = typeof opts !== 'undefined'? opts: {};
+    opts.href = url;
+    for (var key in opts) {
+      var value = opts[key];
+      attrs.push(key + '="' + escape(value) + '"');
+    }
+    return '<a ' + attrs.join(' ') + '>' + title + '</a>';
   },
   'markdown': function (title, url) {
     return '[' + title + '](' + url + ')';
@@ -26,7 +35,7 @@ var formats = {
 module.exports = autolinks;
 module.exports.supportedFormats = Object.keys(formats);
 
-function autolinks(s, format) {
+function autolinks(s, format, opts) {
   var fn;
 
   switch (typeof format) {
@@ -45,8 +54,8 @@ function autolinks(s, format) {
     throw new Error('Unkown format: ' + format);
   }
 
-  return s.replace(URIre, function ($0) { return fn($0, $0); })
-          .replace(incompleteURIre, function ($0, $1, $2) { return $1 + fn($2, 'http://' + $2); })
-          .replace(mailre, function ($0) { return fn($0, 'mailto:' + $0); });
+  return s.replace(URIre, function ($0) { return fn($0, $0, opts); })
+          .replace(incompleteURIre, function ($0, $1, $2) { return $1 + fn($2, 'http://' + $2, opts); })
+          .replace(mailre, function ($0) { return fn($0, 'mailto:' + $0, opts); });
 
 }
